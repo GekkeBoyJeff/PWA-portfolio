9fb01e81488e898b2e6e40b3516c207168a6e136
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2023-03-24 15:25:24 +0100

    update 8 files

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 import express from 'express';
+import fetch from 'node-fetch';
+
 import projectsRouter from './routes/projects.js';
 
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.get('/' ,(req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const username = "GekkeBoyJeff";
+    try {
+        const response = await fetch(`https://gh-pinned-repos.egoist.dev/?username=${username}`);
+        const repos = await response.json();
+        console.log(repos);
+
+        repos.forEach(async repo => {
+            const repoDataResponse = await fetch(`https://raw.githubusercontent.com/${username}/${repo.repo}/main/assets/mockup.webp`);
+            const repoData = await repoDataResponse;
+            console.log(repoData);
+        });
+
+        res.render('index', { repos });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).end();
+    }
 });
 
 app.use('/projects', projectsRouter);
@@ -15,4 +33,4 @@ app.use(express.static('public')); // Static files (css, js, images)
 
 app.listen(port, () =>{
     console.log(`Server is running on port http://127.0.0.1:${port}`)
-});
+})
